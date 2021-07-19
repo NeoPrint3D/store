9860b8cc98d9f7c2415f9567b4650ed682b5783d
NeoPrint777 <drew@ronsman.com>
2021-07-18 20:34:57 -0500

    Updates

--- js/forms.js
+++ js/forms.js
@@ -14,14 +14,12 @@ const btn = document.getElementById('button');
 document.getElementById('form')
  .addEventListener('submit', function(event) {
    event.preventDefault();
-
    btn.value = 'Sending...';
    const encrypt='01110011 01100101 01110010 01110110 01101001 01100011 01100101 01011111 01111001 01110111 00110100 00110011 00110110 00110001 01101010'
    const serviceID = encrypt.split(' ') //Split string in array of binary chars
     .map(bin => String.fromCharCode(parseInt(bin, 2))) //Map every binary char to real char
     .join('');
    const templateID = 'template_zv7jmdd';
-   console.log(serviceID)
    if(ValidateEmail(document.getElementById('reply_to'))){
    emailjs.sendForm(serviceID, templateID, this)
     .then(() => {
